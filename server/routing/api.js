7aa53b1631e811b3c7c8d7c18f1e2a6b1bfc7abd
Med Karim Bchini <mohkarb@gmail.com>
2018-02-20 16:54:42 +0100

    add contact api

--- server/routing/api.js
+++ server/routing/api.js
@@ -1,21 +1,77 @@
 const router = require('express').Router();
-
+const nodemailer = require('nodemailer');
 const ObjectID=require('mongodb').ObjectId;
 const bodyParser=require('body-parser');
 
+const Contact = require('../models/contact');
+
 router.use(bodyParser.json());
 
-router.get('/', (req, res)=>{
+// Response handling
+let response = {
+  status: 200,
+  data: [],
+  message: null
+};
+
+// Error handling
+const sendError = (err, res, code) => {
+  response.status = code;
+  response.message = typeof err == 'object' ? err.message : err;
+  res.status(code).json(response);
+};
+
+
+/*router.get('/', (req, res)=>{
     res.send('Courses API work !')
-});
+});*/
 
 // Send contact us
 router.post('/contact', (req, res)=>{
+let newContact = Contact({
+  name: req.body.name,
+  email: req.body.email,
+  subject: req.body.subject,
+  message: req.body.message
+  });
 
-});
+  console.log('newContact', newContact);
 
-// show contact us
-router.get('/contact', (req, res)=>{
+  newContact.save((err, user) => {
+      if(err)
+          sendError(err,res,500);
+      else {
+          response.data = user;
+          response.message = "OK";
+          res.json(response);
+      }
+
+  });
+
+  // send email
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'youremail@gmail.com',
+      pass: 'yourpassword'
+    }
+  });
+  
+  var mailOptions = {
+    from: req.body.email,
+    to: 'myfriend@yahoo.com',
+    subject: req.body.subject,
+    text: req.body.message
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 
 });
 
