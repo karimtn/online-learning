0492a126d462013171891f55d445af30246bd00b
Med Karim Bchini <mohkarb@gmail.com>
2018-02-20 17:20:17 +0100

    add contact component not completed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  visible = 0;
 
-  constructor() { }
+  contactForm: FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.contactForm = fb.group({
+      'name': ['', Validators.required ],
+      'email': ['', Validators.email, Validators.required],
+      'subject': ['', Validators.required ],
+      'message': ['', Validators.required ]
+    })
+  }
 
   ngOnInit() {
   }
 
+  onClickContact(){
+    this.visible = 1;
+  }
 }
