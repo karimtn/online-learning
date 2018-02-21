134b0c91a6ea5991d653e352aa243a9482a66785
MyAmr <36202563+MyAmr@users.noreply.github.com>
2018-02-21 15:21:21 +0100

    retouche

--- server/routing/guest.js
+++ server/routing/guest.js
@@ -6,7 +6,7 @@ const bodyParser=require('body-parser');
 
 router.use(bodyParser.json());
 
-router.get('/:id', (req, res)=>{//guest courses
+router.get('/:id', (req, res)=>{//search guest courses
     let N={_id:ObjectID(req.params.id)};
     coursesDb.findOne(N).then((rslt)=>{
             response.data=rslt;
@@ -16,8 +16,19 @@ router.get('/:id', (req, res)=>{//guest courses
             sendError(err,res,501);
         })
             res.se('course selected');
-        })
+});
     
+router.get('/courses', (req, res)=>{//guest courses
+    coursesDb.findOne(N).then((rslt)=>{
+            response.data=rslt.title;
+            response.message="ok";
+            res.json(response);
+        }).catch((err)=>{
+            sendError(err,res,501);
+        })
+            res.se('course selected');
+});
+
 
     // Error handling
 const sendError = (err, res, code) => {
