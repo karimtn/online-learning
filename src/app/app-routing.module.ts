742146168af2ca998de414bf4d03c2209c22bd5c
MyAmr <36202563+MyAmr@users.noreply.github.com>
2018-02-21 15:23:29 +0100

    retouche 02

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddcoursesComponent } from './addcourses/addcourses.component';
 import { CommentsComponent } from './comments/comments.component';
-import { ContactusComponent } from './contactus/contactus.component';
+import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
 import { HomeComponent } from './home/home.component';
@@ -15,55 +15,55 @@ import { ViewcoursesComponent } from './viewcourses/viewcourses.component';
 
 const routes: Routes = [
   {
-    path:'addcourses',
-    component:AddcoursesComponent
+    path: 'addcourses',
+    component: AddcoursesComponent
   },
   {
-    path:'comments',
-    component:CommentsComponent
+    path: 'comments',
+    component: CommentsComponent
   },
   {
-    path:'contactus',
-    component:ContactusComponent
+    path: 'contactus',
+    component: ContactComponent
   },
   {
-    path:'courses',
-    component:CoursesComponent
+    path: 'courses',
+    component: CoursesComponent
   },
   {
-    path:'dashbord',
-    component:DashbordComponent
+    path: 'dashbord',
+    component: DashbordComponent
   },
   {
-    path:'',
-    component:HomeComponent
+    path: '',
+    component: HomeComponent
   },
   {
-    path:'login',
-    component:LoginComponent
+    path: 'login',
+    component: LoginComponent
   },
   {
-    path:'menu',
-    component:MenuComponent
+    path: 'menu',
+    component: MenuComponent
   },
   {
-    path:'register',
-    component:RegisterComponent
+    path: 'register',
+    component: RegisterComponent
   },
   {
-    path:'search',
-    component:SearchComponent
+    path: 'search',
+    component: SearchComponent
   },
   {
-    path:'users',
-    component:UsersComponent
+    path: 'users',
+    component: UsersComponent
   },
   {
-    path:'viewcourses',
-    component:ViewcoursesComponent
+    path: 'viewcourses',
+    component: ViewcoursesComponent
   }
 ];
-  
+
   @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
